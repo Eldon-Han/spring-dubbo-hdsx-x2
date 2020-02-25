c89e0dfbbb12aede43edee40311fdec9d773f3f8
liangchengcheng <1038127753@qq.com>
2020-02-25 19:57:53 +0800

    开始调试-product包的测试1

--- dubbo-service-product/product-service-restful/src/main/java/com/hdsx/dubbo/product/ProductRestApplication.java
+++ dubbo-service-product/product-service-restful/src/main/java/com/hdsx/dubbo/product/ProductRestApplication.java
@@ -4,15 +4,14 @@ import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableDubboConfig
 @DubboComponentScan("com.hdsx.dubbo.product.controller")
 @SpringBootApplication
-public class ProductApplication {
+public class ProductRestApplication {
 
 	public static void main(String[] args) {
-	    SpringApplication.run(ProductApplication.class, args);
+	    SpringApplication.run(ProductRestApplication.class, args);
 	}
 
 }
